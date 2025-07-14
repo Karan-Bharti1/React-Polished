de21d847df93678df2d9800ebc92a29dda43e69e
Karan-Bharti1 <karan.codes1@gmail.com>
2025-07-14 13:16:13 +0530

    Passing Multiple children in element in form an array

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{id:"heading"},"Hello World")))
+const parent=React.createElement("div",{id:"parent"},[React.createElement("h1",{id:"child1"},"Hello Child 1"),React.createElement("h2",{id:"child2"},"Hello Child 2")])
 const root=ReactDOM.createRoot(document.getElementById("root"))
  root.render(parent)
-//  <div id="parent">
-//     <div id="child">
-//         <h1 id="heading">Hello World</h1> We get such a structure returned using these nested elements
-//     </div>
-//  </div>
+// Passing Mutiple children in one element in form of an array
