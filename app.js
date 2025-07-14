d9f983d5e23e8e13aba04fefe4c7988aaa5ff885
Karan-Bharti1 <karan.codes1@gmail.com>
2025-07-14 16:56:28 +0530

    npm initialization and installation of parcel

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const parent=React.createElement("div",{id:"parent"},[React.createElement("h1",{id:"child1"},"Hello Child 1"),React.createElement("h2",{id:"child2"},"Hello Child 2")])
+import React from "react"
+import ReactDOM from "react-dom/client"
+const parent=React.createElement("div",{id:"parent"},React.createElement("h1",{id:"child1"},"Hello Child"))
 const root=ReactDOM.createRoot(document.getElementById("root"))
  root.render(parent)
 // Passing Mutiple children in one element in form of an array
