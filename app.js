293098df07faa6c7048b42ef1d96e93cef8a4404
Karan-Bharti1 <karan.codes1@gmail.com>
2025-07-15 10:57:45 +0530

    Adding id or classes to jsx

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-const jsxHeading=<h1>I am Karan</h1>
+const jsxHeading=<h1 id="heading">I am Karan</h1>// jsxHeading also returns object
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(jsxHeading)
