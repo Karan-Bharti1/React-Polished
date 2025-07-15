b6428e0378b5650c6625333b9465f27df70800bd
Karan-Bharti1 <karan.codes1@gmail.com>
2025-07-15 10:55:09 +0530

    Writing jsx in react

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-const parent=React.createElement("div",{id:"parent"},React.createElement("h1",{id:"child1"},"Hello Child"))
+const jsxHeading=<h1>I am Karan</h1>
 const root=ReactDOM.createRoot(document.getElementById("root"))
- root.render(parent)
-// Passing Mutiple children in one element in form of an array
+root.render(jsxHeading)
