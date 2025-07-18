23dc55532b5fa49f56dfb9f037312ca1c9c4cec9
Karan-Bharti1 <karan.codes1@gmail.com>
2025-07-18 12:36:36 +0530

    Fetching Data from swiggy's live api using CORS Plugin

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,41 @@
+import { useEffect, useState } from "react";
 
-import { useState } from "react";
-import { restaurantData } from "../utils/data";
 import RestaurantCard from "./RestaurantCard";
+import { RES_URL } from "../utils/url";
 
 const Body = () => {
-  const [restaurantLists,setRestaurantsList]=useState(restaurantData)
-const handleClick=()=>{
-  setRestaurantsList(()=>restaurantLists.filter(res=>res.info.avgRating>4.5))
-}
+  const [restaurantLists, setRestaurantsList] = useState([]);
+  const handleClick = () => {
+    setRestaurantsList(() =>
+      restaurantLists.filter((res) => res.info.avgRating > 4.5)
+    );
+  };
+  const fetchData = async() => {
+    const data =await  fetch(
+      RES_URL
+    )
+      const json=await data.json()
+      console.log(json.data?.cards)
+    console.log (json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+      setRestaurantsList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  };
+  useEffect(() => {fetchData()
+  console.log(restaurantLists)
+  }, []);
   return (
     <div className="body">
-      <div className="filter"><button onClick={handleClick} className="primary-button">Top Rated Restaurants</button></div>
+      <div className="filter">
+        <button onClick={handleClick} className="primary-button">
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {restaurantLists?.map(data=><RestaurantCard key={data.info.id} resData={data}/>)}
+        {restaurantLists?.map((data) => (
+          <RestaurantCard key={data.info.id} resData={data} />
+        ))}
       </div>
     </div>
   );
 };
+
 export default Body;
