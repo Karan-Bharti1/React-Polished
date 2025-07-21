e4f28d0e2e0c9d776ecfc20ebabaf0e6a2e891e3
Karan-Bharti1 <karan.codes1@gmail.com>
2025-07-21 12:58:50 +0530

    Understood Class based component's Lifecycle

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,15 +1,36 @@
 import React from "react";
-class UserClass extends React.Component{
-    constructor(props){
-        super(props)
-    }
-render(){
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Child Constructor")
+    this.state = {
+      // note this.state will container all the state variables
+      count: 0,
+    };
+  }
+  componentDidMount(){
+    console.log("Component Did Mount")
+  }
+  render() {
+    const { name, email } = this.props;
+    const { count } = this.state;
+     console.log("Child render")
     return (
-        <div>
-            <h1>{this.props.name}</h1>
-            <h1>{this.props.email}</h1>
-        </div>
-    )
+      <div>
+        <h1>{name}</h1>
+        <h1>{email}</h1>
+        <h2>Count: {count}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Set Count
+        </button>
+      </div>
+    );
+  }
 }
-}
-export default UserClass
+export default UserClass;
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,21 @@ import React from "react";
 import Header from "../components/Header";
 import UserClass from "../components/UserClass";
 
-function About() {
-  return (
-    <main className="container">
+class About extends React.Component{
+  constructor(props){
+    super(props)
+    console.log("Parent Constructor")
+  }
+  componentDidMount(){
+    console.log("Parent Component Did Mount")
+  }
+  render() {
+    console.log("Parent Render")
+  return (<main className="container">
       <h2>This is about page</h2>
       <UserClass name="Karan" email="karanbharti.0129@gmail.com" />
-    </main>
-  );
+    </main>)}
 }
+
 
 export default About;
